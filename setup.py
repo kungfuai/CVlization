551485347739f0bbb147d6f03af67926643eb7d7
zz <zhangzhang.si@gmail.com>
2022-09-09 14:00:52 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='cvlization',
+      version='v0.0.6',
+      description='Training pipelines for Computer Vision',
+      author='KUNGFU.AI',
+      author_email='zz@kungfu.ai',
+      url='https://github.com/kungfuai/cvlization',
+      license='MIT',
+      packages=['cvlization'],
+    )
