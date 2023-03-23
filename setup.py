8c6247b1d631adab5b2677da9658f2ce81810304
kungfu <kungfu.ai>
2023-03-23 00:53:22 +0000

    use find_packages to fix relative import

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='cvlization',
       version='0.1.1',
@@ -8,7 +9,8 @@ setup(name='cvlization',
       author_email='zz@kungfu.ai',
       url='https://github.com/kungfuai/cvlization',
       license='MIT',
-      packages=['cvlization'],
+      # packages=['cvlization'],
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
     )
