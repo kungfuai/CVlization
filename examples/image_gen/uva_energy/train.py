25dde84c2258e422aadfc9eaac55578457b2d414
ZZ <zhangzhang.si@gmail.com>
2023-04-09 02:45:10 +0000

    make uva energy model work on flowers102

--- examples/image_gen/uva_energy/train.py
+++ examples/image_gen/uva_energy/train.py
@@ -1,14 +1,49 @@
-from torchvision.datasets import MNIST
+from argparse import ArgumentParser
 from cvlization.torch.training_pipeline.image_gen.ebm.uva_energy.pipeline import TrainingPipeline
 
-DATASET_PATH = "./data/raw/MNIST"
 
 class MNISTDatasetBuilder:
+    dataset_path = "./data/raw/MNIST"
+
     def training_dataset(self):
-        return MNIST(root=DATASET_PATH, train=True, download=True)
+        return MNIST(root=self.dataset_path, train=True, download=True)
 
     def validation_dataset(self):
-        return MNIST(root=DATASET_PATH, train=False, download=True)
+        return MNIST(root=self.dataset_path, train=False, download=True)
 
 
-TrainingPipeline().fit(MNISTDatasetBuilder())
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("--dataset", "-d", type=str, default="mnist", help="dataset name")
+    arg_parser.add_argument("--image_shape", "-s", type=str, help="image shape: e.g. 1,28,28", default="1,28,28")
+    arg_parser.add_argument("--epochs", "-e", type=int, default=60, help="number of epochs")
+    arg_parser.add_argument("--batch_size", "-b", type=int, default=128, help="batch size")
+    args = arg_parser.parse_args()
+
+    img_shape = tuple(map(int, args.image_shape.split(",")))
+    if args.dataset.lower() == "mnist":
+        from torchvision.datasets import MNIST
+
+        dataset_builder = MNISTDatasetBuilder()
+    elif "/" in args.dataset:
+        from cvlization.data.huggingface import HuggingFaceDatasetBuilder
+
+        if args.dataset in ["huggan/flowers-102-categories"]:
+            validation_split_name = "train"
+            img_shape = (3, img_shape[1], img_shape[2])
+        else:
+            validation_split_name = "validation"
+        dataset_builder = HuggingFaceDatasetBuilder(
+            args.dataset,
+            validation_split_name=validation_split_name,
+        )
+    else:
+        raise ValueError(f"Unknown dataset {args.dataset}")
+    
+    
+    TrainingPipeline(
+        img_shape=img_shape,
+        epochs=args.epochs,
+        train_batch_size=args.batch_size,
+        val_batch_size=args.batch_size,
+    ).fit(dataset_builder)
