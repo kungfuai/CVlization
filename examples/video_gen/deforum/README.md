7a9be90f6d362d5d360407a96af861153a13acf5
ZZ Si <zhangzhang.si@gmail.com>
2023-11-24 10:43:01 -0600

    deforum wip

--- examples/video_gen/deforum/README.md
+++ examples/video_gen/deforum/README.md
@@ -1,1 +1,7 @@
 Adapted from: https://github.com/deforum-art/deforum-stable-diffusion/blob/main/Deforum_Stable_Diffusion.py
+
+```
+pip install git+https://github.com/openai/CLIP.git
+pip install jsonmerge clean-fid resize-right torchdiffeq torchsde pydantic omegaconf
+pip install open-clip-torch numexpr
+```
--- examples/video_gen/deforum/helpers/ffmpeg_helpers.py
+++ examples/video_gen/deforum/helpers/ffmpeg_helpers.py
@@ -4,17 +4,19 @@ from base64 import b64encode
 from IPython import display
 import cv2
 
-def get_extension_maxframes(args,ffmpeg_outdir,ffmpeg_timestring):
+
+def get_extension_maxframes(args, ffmpeg_outdir, ffmpeg_timestring):
     all_frames = []
     for file in os.listdir(args.outdir):
         if file.startswith(args.timestring):
-            if file.endswith(('.png', '.jpg', '.jpeg', '.exr')):
+            if file.endswith((".png", ".jpg", ".jpeg", ".exr")):
                 all_frames.append(file)
     extension = all_frames[0].split(".")[-1]
     max_frames = len(all_frames)
     return extension, max_frames
 
-def get_auto_outdir_timestring(args,ffmpeg_mode):
+
+def get_auto_outdir_timestring(args, ffmpeg_mode):
     try:
         ffmpeg_outdir = args.outdir
         ffmpeg_timestring = args.timestring
@@ -23,44 +25,59 @@ def get_auto_outdir_timestring(args,ffmpeg_mode):
         print(e)
         print("ffmpeg mode set to auto and args not defined in global scope")
 
+
 def get_ffmpeg_path(ffmpeg_outdir, ffmpeg_timestring, ffmpeg_extension):
     try:
-        ffmpeg_image_path = os.path.join(ffmpeg_outdir,ffmpeg_timestring+f"_%05d.{ffmpeg_extension}")
-        ffmpeg_mp4_path = os.path.join(ffmpeg_outdir,ffmpeg_timestring+".mp4")
-        ffmpeg_gif_path = os.path.join(ffmpeg_outdir,ffmpeg_timestring+".gif")
+        ffmpeg_image_path = os.path.join(
+            ffmpeg_outdir, ffmpeg_timestring + f"_%05d.{ffmpeg_extension}"
+        )
+        ffmpeg_mp4_path = os.path.join(ffmpeg_outdir, ffmpeg_timestring + ".mp4")
+        ffmpeg_gif_path = os.path.join(ffmpeg_outdir, ffmpeg_timestring + ".gif")
         return ffmpeg_image_path, ffmpeg_mp4_path, ffmpeg_gif_path
     except Exception as e:
         print(e)
         print("error making path variables redefine args or use different mode")
 
-def make_mp4_ffmpeg(ffmpeg_args, display_ffmpeg=False, debug=False):
 
+def make_mp4_ffmpeg(ffmpeg_args, display_ffmpeg=False, debug=False):
     if os.path.exists("./ffmpeg"):
-        ffmpeg_path = './ffmpeg'
+        ffmpeg_path = "./ffmpeg"
     else:
-        ffmpeg_path = 'ffmpeg'
+        ffmpeg_path = "ffmpeg"
 
     # make mp4
     cmd = [
         ffmpeg_path,
-        '-y',
-        '-vcodec', "png",
-        '-r', str(ffmpeg_args.ffmpeg_fps),
-        '-start_number', str(0),
-        '-i', ffmpeg_args.ffmpeg_image_path,
-        '-frames:v', str(ffmpeg_args.ffmpeg_maxframes),
-        '-c:v', 'libx264',
-        '-vf',
-        f'fps={ffmpeg_args.ffmpeg_fps}',
-        '-pix_fmt', 'yuv420p',
-        '-crf', '17',
-        '-preset', 'veryfast',
-        '-pattern_type', 'sequence',
-        ffmpeg_args.ffmpeg_mp4_path
+        "-y",
+        "-vcodec",
+        "png",
+        "-r",
+        str(ffmpeg_args.ffmpeg_fps),
+        "-start_number",
+        str(0),
+        "-i",
+        ffmpeg_args.ffmpeg_image_path,
+        "-frames:v",
+        str(ffmpeg_args.ffmpeg_maxframes),
+        "-c:v",
+        "libx264",
+        "-vf",
+        f"fps={ffmpeg_args.ffmpeg_fps}",
+        "-pix_fmt",
+        "yuv420p",
+        "-crf",
+        "17",
+        "-preset",
+        "veryfast",
+        "-pattern_type",
+        "sequence",
+        ffmpeg_args.ffmpeg_mp4_path,
     ]
 
-    if debug:
-        print(cmd)
+    # if debug:
+    print("=" * 80)
+    print(cmd)
+    print("=" * 80)
 
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
@@ -69,34 +86,46 @@ def make_mp4_ffmpeg(ffmpeg_args, display_ffmpeg=False, debug=False):
         raise RuntimeError(stderr)
 
     if display_ffmpeg == True:
-        mp4 = open(ffmpeg_args.ffmpeg_mp4_path,'rb').read()
+        mp4 = open(ffmpeg_args.ffmpeg_mp4_path, "rb").read()
         data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-        display.display(display.HTML(f'<video controls loop><source src="{data_url}" type="video/mp4"></video>'))
+        display.display(
+            display.HTML(
+                f'<video controls loop><source src="{data_url}" type="video/mp4"></video>'
+            )
+        )
 
 
 def make_gif_ffmpeg(ffmpeg_args, debug=False):
-
     if os.path.exists("./ffmpeg"):
-        ffmpeg_path = './ffmpeg'
+        ffmpeg_path = "./ffmpeg"
     else:
-        ffmpeg_path = 'ffmpeg'
+        ffmpeg_path = "ffmpeg"
 
     # make gif
     cmd = [
         ffmpeg_path,
-        '-y',
-        '-vcodec', "png",
-        '-r', str(ffmpeg_args.ffmpeg_fps),
-        '-start_number', str(0),
-        '-i', ffmpeg_args.ffmpeg_image_path,
-        '-frames:v', str(ffmpeg_args.ffmpeg_maxframes),
-        '-vf',
-        f'fps={ffmpeg_args.ffmpeg_fps}',
-        '-pix_fmt', 'yuv420p',
-        '-crf', '17',
-        '-preset', 'veryfast',
-        '-pattern_type', 'sequence',
-        ffmpeg_args.ffmpeg_gif_path
+        "-y",
+        "-vcodec",
+        "png",
+        "-r",
+        str(ffmpeg_args.ffmpeg_fps),
+        "-start_number",
+        str(0),
+        "-i",
+        ffmpeg_args.ffmpeg_image_path,
+        "-frames:v",
+        str(ffmpeg_args.ffmpeg_maxframes),
+        "-vf",
+        f"fps={ffmpeg_args.ffmpeg_fps}",
+        "-pix_fmt",
+        "yuv420p",
+        "-crf",
+        "17",
+        "-preset",
+        "veryfast",
+        "-pattern_type",
+        "sequence",
+        ffmpeg_args.ffmpeg_gif_path,
     ]
 
     if debug:
@@ -108,27 +137,34 @@ def make_gif_ffmpeg(ffmpeg_args, debug=False):
         print(stderr)
         raise RuntimeError(stderr)
 
-def patrol_cycle(args,ffmpeg_args, debug=False):
-        cap = cv2.VideoCapture(ffmpeg_args.ffmpeg_mp4_path)
-        # Get the video frames
-        frames = []
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            frames.append(frame)
 
-        # Reverse the frames
-        reversed_frames = frames[::-1]
+def patrol_cycle(args, ffmpeg_args, debug=False):
+    cap = cv2.VideoCapture(ffmpeg_args.ffmpeg_mp4_path)
+    # Get the video frames
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
 
-        # Append the original frames to the reversed frames
-        frames = frames + reversed_frames
-        # Create a video writer object
-        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        out = cv2.VideoWriter(ffmpeg_args.ffmpeg_mp4_path, fourcc, float(ffmpeg_args.ffmpeg_fps), (args.W, args.H), isColor=True)
-        # Write the frames to the output video
-        for frame in frames:
-            out.write(frame)
-        # Release the video writer and capture objects
-        out.release()
-        cap.release()
+    # Reverse the frames
+    reversed_frames = frames[::-1]
+
+    # Append the original frames to the reversed frames
+    frames = frames + reversed_frames
+    # Create a video writer object
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter(
+        ffmpeg_args.ffmpeg_mp4_path,
+        fourcc,
+        float(ffmpeg_args.ffmpeg_fps),
+        (args.W, args.H),
+        isColor=True,
+    )
+    # Write the frames to the output video
+    for frame in frames:
+        out.write(frame)
+    # Release the video writer and capture objects
+    out.release()
+    cap.release()
