ce670c8d313f7a64005fed9ecbd0cec270ba9e94
ZZ <zz@kungfu.ai>
2024-02-08 14:11:54 -0600

    bump to 0.1.4

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='cvlization',
-      version='0.1.3',
-      description='Training pipelines for Computer Vision',
+      version='0.1.4',
+      description='Training pipelines for Vision, Language and more modalities.',
       author='KUNGFU.AI',
       author_email='zz@kungfu.ai',
       url='https://github.com/kungfuai/cvlization',
       license='MIT',
-      # packages=['cvlization'],
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
