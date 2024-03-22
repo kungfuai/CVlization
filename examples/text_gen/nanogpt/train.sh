c853cf64166ac3d173af72afce0d21c8c9e98dff
ZZ <zz@kungfu.ai>
2024-03-21 19:40:21 -0500

    nanogpt: refactor

--- examples/text_gen/nanogpt/train.sh
+++ examples/text_gen/nanogpt/train.sh
@@ -1,5 +1,6 @@
 docker run --runtime nvidia -it \
 	-v $(pwd)/examples/text_gen/nanogpt:/workspace \
 	-v $(pwd)/data/container_cache:/root/.cache \
+	-v $(pwd)/cvlization:/workspace/cvlization \
 	nanogpt \
 	python train.py config/train_shakespeare_char.py
