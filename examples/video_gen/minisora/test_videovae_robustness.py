61c518962d6ea293c84db66d8007477720931466
ZZ Si <zhangzhang.si@gmail.com>
2024-04-28 06:08:36 +0000

    add vae variant

--- examples/video_gen/minisora/test_videovae_robustness.py
+++ examples/video_gen/minisora/test_videovae_robustness.py
@@ -3,17 +3,19 @@ import torch
 import numpy as np
 from diffusers.models import AutoencoderKL
 from cvlization.dataset.flying_mnist import FlyingMNISTDatasetBuilder
+from cvlization.torch.net.vae.video_vqvae import VQVAE
 
-vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse")
+vae = VQVAE.from_pretrained("zzsi_kungfu/videogpt/model-kbu39ped:v11")
 vae.eval()
 
 # create a sample batch
 batch_size = 2
 height = 64
 width = 64
+num_frames = 4
 num_channels = 3
 # x = torch.ones(batch_size, num_channels, height, width)
-x = torch.randn(batch_size, num_channels, height, width) * 100
+x = torch.randn(batch_size, num_channels, num_frames, height, width) * 100
 
 device = "cuda"
 vae = vae.to(device)
@@ -22,7 +24,7 @@ for i in range(3):
     # run the model
     with torch.no_grad():
         out = vae.encode(x)
-        z = out.latent_dist.sample()
+        z = out
         print(f"repetition {i+1}", z.mean(), z.min(), z.max())
     
 
@@ -30,26 +32,21 @@ db = FlyingMNISTDatasetBuilder(resolution=256, max_frames_per_video=16, dataset_
 ds = db.training_dataset()
 print(len(ds), "examples")
 vid = ds[3]["video"]
-i_frame = 10
-vid = vid.unsqueeze(0).to(device)[:, :, i_frame, :, :] # take the i_frame-th frame
+vid = vid.unsqueeze(0).to(device)[:, :, :, :, :]
 print(vid.shape, vid[0, 0].mean().item())
 # save the first frame
 frame = vid[0].detach().cpu()
-print(f"frame mean: {frame.mean().item()}")
-frame = frame.permute(1, 2, 0).numpy()
-frame = (frame - frame.min()) / (frame.max() - frame.min() + 1e-6)
-frame = (frame * 255).astype(np.uint8)
-Image.fromarray(frame).save("data/tmp_frame.png")
 
 with torch.no_grad():
-    z = vae.encode(vid).latent_dist.sample()
+    z = vae.encode(vid)
+    z = vae.vq(z)["z_recon"]
+    # corrupt the latents
+    z[:, :, 1:, :, :] = torch.randn_like(z[:, :, 1:, :, :])
     print(z.shape, z.mean(), z.min(), z.max())
-    # corrupt the bottom half of the latent
-    z[:, :, 26:, :] = torch.randn_like(z[:, :, 26:, :]) * 5
     decoded = vae.decode(z)
-    print("decoded:", decoded.sample.shape)
+    print("decoded:", decoded.shape)
     # save image to png
-    recon_img = decoded.sample.detach().cpu()
+    recon_img = decoded.detach().cpu()[:, :, 2, :, :]
     print("recon:", recon_img.shape, recon_img.mean().item())
     recon_img = recon_img[0].permute(1, 2, 0).numpy()
     recon_img = (recon_img - recon_img.min()) / (recon_img.max() - recon_img.min() + 1e-6)
