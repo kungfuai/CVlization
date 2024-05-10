3597a2bfc4a37c4dcc96edb77933f873ce5c54a1
ZZ Si <zhangzhang.si@gmail.com>
2024-05-10 02:16:42 +0000

    fix mamba training

--- examples/text_gen/nanomamba/train.sh
+++ examples/text_gen/nanomamba/train.sh
@@ -2,6 +2,7 @@ docker run --runtime nvidia -it \
 	-v $(pwd):/workspace \
 	-v $(pwd)/data/container_cache:/root/.cache \
 	-e PYTHONPATH=.:examples/text_gen/nanomamba \
+	-e WANDB_API_KEY=$WANDB_API_KEY \
 	nanomamba \
 	python examples/text_gen/nanomamba/train.py $@
 
