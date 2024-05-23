4bab7df5731b1a705e03c17e1b97a599902f68c4
ZZ Si <zhangzhang.si@gmail.com>
2024-05-23 01:51:36 +0000

    dreambooth

---
+++ examples/image_gen/dreambooth/train.sh
@@ -0,0 +1,5 @@
+docker run --runtime nvidia -it \
+	-v $(pwd):/workspace \
+	-v $(pwd)/data/container_cache:/root/.cache \
+	dreambooth \
+	python -m examples.image_gen.dreambooth.train --save_sample_prompt "a happy sks cat" $@
