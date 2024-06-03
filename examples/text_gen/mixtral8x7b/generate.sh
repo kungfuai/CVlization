e9a06add118a7220566aeb948766e01a857e2a84
ZZ Si <zhangzhang.si@gmail.com>
2024-06-03 02:10:55 +0000

    mixtral finetune: add hf token env

--- examples/text_gen/mixtral8x7b/generate.sh
+++ examples/text_gen/mixtral8x7b/generate.sh
@@ -1,5 +1,6 @@
 docker run --runtime nvidia -it \
 	-v $(pwd)/examples/text_gen/mixtral8x7b/generate.py:/workspace/generate.py \
 	-v $(pwd)/data/container_cache:/root/.cache \
+	-e HF_TOKEN=$HF_TOKEN \
 	mixtral \
 	python3 generate.py
