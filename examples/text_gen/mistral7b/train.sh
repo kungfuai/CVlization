61ae05ef4e6ba5b4a5ebeba9903bb51f30fe1934
ZZ Si <zhangzhang.si@gmail.com>
2024-06-03 02:10:55 +0000

    mixtral finetune: add hf token env

--- examples/text_gen/mistral7b/train.sh
+++ examples/text_gen/mistral7b/train.sh
@@ -1,5 +1,6 @@
 docker run --runtime nvidia -it \
 	-v $(pwd)/examples/text_gen/mistral7b:/workspace \
 	-v $(pwd)/data/container_cache:/root/.cache \
+	-e HF_TOKEN=$HF_TOKEN \
 	mistral \
 	python3 train.py
