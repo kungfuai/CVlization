d772ca62c45daa9b1050d4c8df23a835625442c0
ZZ Si <zhangzhang.si@gmail.com>
2024-10-01 16:50:26 +0000

    adjust wandb for diffuser-unconditional

--- examples/image_gen/diffuser_unconditional/train.sh
+++ examples/image_gen/diffuser_unconditional/train.sh
@@ -3,5 +3,7 @@ module=examples.image_gen.diffuser_unconditional.train
 docker run --runtime nvidia --rm \
     -v $(pwd):/workspace \
     -v ./data/container_cache:/root/.cache \
+    -e WANDB_API_KEY=$WANDB_API_KEY \
+    -e WANDB_PROJECT=diffuser-unconditional \
     $image \
     python -m $module $@
