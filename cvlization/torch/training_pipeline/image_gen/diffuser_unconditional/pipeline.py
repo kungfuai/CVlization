9a6f55802ec57f87ea358b279107e6acb53c3a08
ZZ <>
2024-10-04 22:34:22 +0000

    train5 instrumentation

--- cvlization/torch/training_pipeline/image_gen/diffuser_unconditional/pipeline.py
+++ cvlization/torch/training_pipeline/image_gen/diffuser_unconditional/pipeline.py
@@ -32,7 +32,7 @@ from diffusers.optimization import get_scheduler
 from diffusers.training_utils import EMAModel
 from diffusers.utils import check_min_version, is_accelerate_version, is_tensorboard_available, is_wandb_available
 from diffusers.utils.import_utils import is_xformers_available
-from cvlization.torch.training_pipeline.dit.dit import DiT
+from cvlization.torch.training_pipeline.image_gen.dit import DiT
 
 
 # Will error if the minimal version of diffusers is not installed. Remove at your own risks.
