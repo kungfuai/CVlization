bda22fc6e99d4c34b71dac62ba23669087ead3f4
ZZ Si <zhangzhang.si@gmail.com>
2025-05-25 21:42:47 +0000

    wan: improve cli

--- examples/video_gen/wan_comfy/predict.sh
+++ examples/video_gen/wan_comfy/predict.sh
@@ -2,8 +2,9 @@ docker run --shm-size 16G --runtime nvidia -it \
 	-v $(pwd)/examples/video_gen/wan_comfy/:/workspace \
 	-v $(pwd)/data/container_cache:/root/.cache \
 	-v $(pwd)/examples/video_gen/animate_x/data/images:/workspace/examples/video_gen/animate_x/data/images \
+	-v $(pwd)/examples/video_gen/mimic_motion/example_data:/workspace/examples/video_gen/mimic_motion/example_data \
     -e CUDA_VISIBLE_DEVICES='0' \
 	wan_comfy \
-    python predict.py
+    python predict.py "$@"
 	
 	
