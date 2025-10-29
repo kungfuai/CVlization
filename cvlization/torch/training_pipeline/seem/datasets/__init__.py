7cc205e7f6fcd99277ecd854dff213e503909cb3
ZZ Si <zhangzhang.si@gmail.com>
2025-10-28 23:46:47 -0500

    Remove unused SEEM training pipeline

--- cvlization/torch/training_pipeline/seem/datasets/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import registration
-from .build import build_train_dataloader, build_eval_dataloader, build_evaluator
--- cvlization/torch/training_pipeline/seem/modeling/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-from .point_features import *
-from .position_encoding import *
-from .postprocessing import *
-from .attention import *
-from .criterion import *
-from .matcher import *
