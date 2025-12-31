fd1257df448d557efd3f0ff32cf42369176414e3
ZZ Si <zhangzhang.si@gmail.com>
2025-12-31 20:09:50 +0000

    Add SkyPilot multi-cloud runner

--- cvl/runners/__init__.py
+++ cvl/runners/__init__.py
@@ -5,5 +5,6 @@ from .lambda_labs import LambdaLabsRunner
 from .sagemaker_runner import SageMakerRunner
 from .docker_context import DockerContextRunner
 from .k8s_runner import K8sRunner
+from .skypilot_runner import SkyPilotRunner
 
-__all__ = ["SSHRunner", "LambdaLabsRunner", "SageMakerRunner", "DockerContextRunner", "K8sRunner"]
+__all__ = ["SSHRunner", "LambdaLabsRunner", "SageMakerRunner", "DockerContextRunner", "K8sRunner", "SkyPilotRunner"]
